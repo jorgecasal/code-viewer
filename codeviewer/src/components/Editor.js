44b94a547ee0fb7d928cbe0c8dab5428fa7c0138
Jorge Casal <casaldelacruz@gmail.com>
2020-09-22 14:34:10 +0200

    icons

--- codeviewer/src/components/Editor.js
+++ codeviewer/src/components/Editor.js
@@ -1,20 +1,25 @@
-import React from 'react'
+import React, { useState }from 'react'
 import 'codemirror/lib/codemirror.css'
 import 'codemirror/theme/elegant.css'
 import 'codemirror/mode/xml/xml'
 import 'codemirror/mode/css/css'
 import 'codemirror/mode/javascript/javascript'
 import { Controlled as ControlledEditor } from 'react-codemirror2'
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { faMinusSquare, faPlusSquare} from '@fortawesome/free-solid-svg-icons'
 
 export default function Editor({displayName, language, value, onChange}) {
+    const [ open, setOpen ] = useState(true);
     function handleChange(editor, data, value){
         onChange(value)
     }
     return (
-        <div className="editor-container">
+        <div className={`editor-container ${open ? '' : 'collapsed'}`}>
             <div className="editor-title">
                 {displayName}
-                <button>O/C</button>
+                <button className="btn" onClick={()=> setOpen(prevOpen => !prevOpen)}>
+                <FontAwesomeIcon icon={ open ? faMinusSquare : faPlusSquare}/>
+                </button>
             </div>
             <ControlledEditor
                 className="code-mirror-wrapper"

7265c6dc6a49c0b6c94eb8c1e3fe7a894f260592
Jorge Casal <casaldelacruz@gmail.com>
2020-09-23 09:25:27 +0200

    build moved

---
+++ build/precache-manifest.3d8d33ef999ce3aad86cecc52b9759a9.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c540e761c261ab7211b0e20d4f1e7e9e",
+    "url": "/index.html"
+  },
+  {
+    "revision": "ecd7cab37fcaab0f4e50",
+    "url": "/static/css/2.7f733b95.chunk.css"
+  },
+  {
+    "revision": "b7160e87fd754516bc64",
+    "url": "/static/css/main.50541fef.chunk.css"
+  },
+  {
+    "revision": "ecd7cab37fcaab0f4e50",
+    "url": "/static/js/2.8d6be493.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.8d6be493.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "b7160e87fd754516bc64",
+    "url": "/static/js/main.38cec4cf.chunk.js"
+  },
+  {
+    "revision": "43938fa676cb755e1981",
+    "url": "/static/js/runtime-main.8f870dc8.js"
+  }
+]);
